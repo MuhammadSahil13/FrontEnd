dd62d7897ac00667d4d95081f7d52b6bf2c4e56a
Sahil <msahil@visteon.com>
2024-03-12 20:10:35 +0530

    checking the changes

--- src/Components/heading.jsx
+++ src/Components/heading.jsx
@@ -5,7 +5,7 @@ function heading(props){
    
     return(
         <>
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Sahil</span> ECHO</h1>
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Yoo</span> Whatsup</h1>
         </>
     )
 }
